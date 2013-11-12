642680d1fc93d215657a1e2701e0f63fa9d4f4bf
newtonry <vonkunesnewton@gmail.com>
2013-11-12 11:11:41 -0800

    Make graph responsive, add labels

--- app/assets/javascripts/recipes/show.js
+++ app/assets/javascripts/recipes/show.js
@@ -1,22 +1,29 @@
 var addToGraph = function(id, name, time, totalTime) {
-  $("#graph").append("<div id='ingredient" + id + "' class='ingredient-slider'></div>");
+	$("#graph-ingredient-names").append(name);
+  $("#graph").append("<div id='graph-name" + id + "' class='graph-ingredient-name col-md-2'>" + name + "</div>");
+	$("#graph").append("<div id='ingredient" + id + "' class='ingredient-slider col-md-10'></div>");
 
 
 	$("#ingredient" + id).slider({
 		value: time,
-		max: totalTime,
-		min: 0,
+		max: totalTime + 1,
+		min: -1,
 		slide: function(	) {
 			//adjusts the 
-			$('[data-ingredientId="' + id + '"]').find('.ingredient-time').html($("#ingredient" + id).slider( "value"))
+			$('[data-ingredientId="' + id + '"]').find('.ingredient-time').html($("#ingredient" + id).slider( "value"));
 		}
 	});
 	$("#ingredient" + id).slider('disable');	
 };
 
 var makeRecipeEditable = function(event) {
+	// this.recipeBeforeFor = 
 
-	$("#graph").children().slider('enable');
+
+	// $("#graph").children().slider('enable');
+
+	$("#graph").find('.ingredient-slider').slider('enable');
+	
 	$('.ingredient-attr').attr("contentEditable", "true");
 	$("#edit-button").hide('fast');
 	$("#save-button").show('fast');
@@ -55,25 +62,49 @@ var saveRecipe = function(event) {
 	
 	
 	
-	
 	//highlight out button
 
 	makeRecipeUneditable();
 }
 
+var cancelSaveRecipe = function() {
+	
+	
+	makeRecipeUneditable();
+};
+
+
+var addChangeListeners = function(id) {
+	//if the time is changed, need to update the graph as well
+	$('[data-ingredientId="' + id + '"]').find('.ingredient-time').blur(function(){
+		var newValue = $('[data-ingredientId="' + id + '"]').find('.ingredient-time').html();
+		$slider = $("#ingredient" + id).find('a');
+		// $slider.css('left', newValue + '%');
+		$slider.animate({
+			left: newValue + '%'
+		}, 250, function(){
+			//animation complete callback
+		});
+	
+	});
+	
+	$('[data-ingredientId="' + id + '"]').find('.ingredient-name').blur(function(){
+		var newName = $('[data-ingredientId="' + id + '"]').find('.ingredient-name').html();
+		// debugger
+		$nameInGraph = $("#graph-name" + id).html(newName);
+	
+	});
+};
+
+
+
 var makeRecipeUneditable = function(event) {
-	$("#graph").children().slider('disable');
+	// $("#graph").children().slider('disable');
+	$("#graph").find('.ingredient-slider').slider('disable');
+
 	$('.ingredient-attr').attr("contentEditable", "false");
 	$("#save-button").hide('fast');	
 	$("#cancel-save-button").hide('fast');
 	$("#edit-button").show('fast');
 	$("#ingredient-table").toggleClass("highlighted-table");
 }
-
-
-
-
-
-
-
-
